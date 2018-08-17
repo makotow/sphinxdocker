602d97f998b88e980d173d566b07ec0803d51235
Makoto Watanabe <makoto.wata@gmail.com>
2018-08-17 10:37:51 +0900

    Spinx version up.

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@
 
 doc_dir="$1"
 work_dir="$(pwd)/${doc_dir}"
-docker run -v "${work_dir}":/docs --rm -it sphinx-docker:latest sphinx-quickstart
+docker run -v "${work_dir}":/docs --rm -it makotow/sphinx-docker:1.3-sphinx1.7.6 sphinx-quickstart
