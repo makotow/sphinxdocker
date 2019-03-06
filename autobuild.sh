a711b17c789d1c91da4615ab0ab421d068ae6685
Makoto Watanabe <makoto.wata@gmail.com>
2019-03-06 17:44:57 +0900

    Aggreage utility scripts to Makefile.

--- autobuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-doc_dir="$1"
-work_dir="$(pwd)/${doc_dir}"
-docker run -p 8000:8000 -v "${work_dir}":/docs --rm -it makotow/sphinx-docker:1.3-sphinx1.7.63-sphinx1.7.63-sphinx1.7.6 sphinx-autobuild --host 0.0.0.0 source docs/_build/html
